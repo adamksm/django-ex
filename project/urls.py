7139fdf041a513eae22682563fd798b524f24830
El Kassimi <a.elkassimi@uu.nl>
2025-09-18 09:51:10 +0200

    Update requirements for Django compatibility

--- project/urls.py
+++ project/urls.py
@@ -1,21 +1,25 @@
+
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.contrib import admin
+
 
 from welcome.views import index, health
 
+
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # re_path(r'^$', 'project.views.home', name='home'),
+    # re_path(r'^blog/', include('blog.urls')),
 
-    url(r'^$', index),
-    url(r'^health$', health),
-    url(r'^admin/', include(admin.site.urls)),
+    re_path(r'^$', index),
+    re_path(r'^health$', health),
+    re_path(r'^admin/', admin.site.urls),
 ]
+
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
+        re_path(r'^__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
